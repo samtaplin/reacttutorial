d0f08fc22c66daec345a905de0cb6ef2d41327c4
Samuel Taplin <samtaplin@berkeley.edu>
2021-09-08 00:27:22 -0700

    fixed winner

--- src/index.js
+++ src/index.js
@@ -82,12 +82,19 @@ class Game extends React.Component {
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current);
+    const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
         const desc = move ?
           'Go to move #' + move :
           'Go to game start';
+        if (this.state.stepNumber === move) {
+          return (
+            <li key={move}>
+              <button onClick={() => this.jumpTo(move)}><b>{desc}</b></button>
+            </li>
+          );
+        }
         return (
           <li key={move}>
             <button onClick={() => this.jumpTo(move)}>{desc}</button>
